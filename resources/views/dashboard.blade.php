ad493fbdc0c8e57b194e944dcee9bffe23724051
taniot <gaetano.frascolla@gmail.com>
2023-05-24 09:08:52 +0200

    style: dashboard width

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('page.main')
-<div class="container-fluid mt-4">
+<div class="container mt-4">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
